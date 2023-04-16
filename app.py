366cab61e88187fd5ce67e8367e08daf0d14b25f
sivasankarstorage <84306456+sivasankarstorage@users.noreply.github.com>
2023-04-16 18:08:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 import pandas as pd
 import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+import jsonpickle
+
 app = Flask(__name__)
 
 model = joblib.load('rf_model.joblib')
