83ffe244ca83ee38c18dd6c0463abee6016201de
sivasankarstorage <84306456+sivasankarstorage@users.noreply.github.com>
2023-04-16 17:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+import pandas as pd
+import joblib
 app = Flask(__name__)
+
+model = joblib.load('rf_model.joblib')
 
 @app.route('/')
 def hello_world():
@@ -8,3 +12,11 @@ def hello_world():
 @app.route('/name')
 def temp():
     return 'dfsdfsdfdsf'
+
+@app.route('/test')
+def test():
+    input1 = pd.read_csv("test.csv")
+    prediction = model.predict(input1)
+    print(input1)
+    print(prediction)
+    return ' '.join([str(elem) for i,elem in enumerate(prediction)])
