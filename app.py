5cada1f76993e2c55faa754235d67173d5ff34d2
sivasankarstorage <84306456+sivasankarstorage@users.noreply.github.com>
2023-04-16 23:03:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
@@ -14,7 +14,7 @@ model = joblib.load('rf_model.joblib')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Success123!'
+    return render_template('form.html')
 
 @app.route('/name')
 def temp():
@@ -23,6 +23,7 @@ def temp():
 @app.route('/test')
 def test():
     input1 = pd.read_csv("test.csv")
+    print(input1)
     prediction = model.predict(input1)
     print(input1)
     print(prediction)
@@ -30,4 +31,12 @@ def test():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return request
+    # data = request.get_json() 
+    # name = data['name'] 
+    # return f"Hello, {name}!"
+    data = request.data.decode('utf-8').split(' ')
+    print(data)
+    dict= { 'Discount': [int(data[0])], 'Approver':[int(data[1])], 'days':[int(data[2])] } 
+    print(dict)
+    prediction = model.predict(dict)
+    return ' '.join([str(elem) for i,elem in enumerate(prediction)])
